e5d617bc0a1ac5206773f1f359da8804da845c05
Ефим <fimakrut@mail.ru>
2024-04-28 15:48:46 +0500

    Создан класс Children

---
+++ Kursovoy/Classes/Children.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursovoy.Classes
+{
+    public class Child
+    {
+        public int id { get; set; }
+        public string fioChild { get; set; }
+        public string dob { get; set; }
+        public string fio { get; set; }
+        public string groupChild { get; set; }
+        public int parent_id { get; set; }
+    }
+}
