da4e8b9d6c06d90c814a2fab0e1ffbb63c0f28f8
Ефим <fimakrut@mail.ru>
2024-04-28 16:01:58 +0500

    Реализованны необходимые классы

---
+++ Kursovoy/Classes/Applications.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursovoy.Classes
+{
+    public class Applications
+    {
+        public int id { get; set; }
+        public string fioChild { get; set; }
+        public string img_certificate { get; set; }
+        public string parent { get; set; }
+        public string dob { get; set; }
+        public int parent_id { get; set; }
+    }
+}
---
+++ Kursovoy/Classes/Groups.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursovoy.Classes
+{
+    public class Groups
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string FIO { get; set; }
+        public int idEducator { get; set; }
+        public int cabinet { get; set; }
+    }
+}
