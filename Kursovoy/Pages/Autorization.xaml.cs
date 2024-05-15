711cafba2a7113e317c972e8eec06633e2c28189
Ефим <fimakrut@mail.ru>
2024-05-15 20:53:53 +0500

    Создано окно авторизации и регистрации(Внещний вид доработать!)

---
+++ Kursovoy/Pages/Autorization.xaml.cs
@@ -0,0 +1,89 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using Kursovoy.Classes;
+
+
+
+namespace Kursovoy.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        MainWindow mainWindow;
+        //int IdParent;
+        public Authorization(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+
+            if (File.Exists(mainWindow.localPath + @"\save.txt") && File.ReadAllText(mainWindow.localPath + @"\save.txt") != "")
+            {
+                StreamReader sr = new StreamReader(mainWindow.localPath + @"\save.txt");
+                try
+                {
+                    email.Text = sr.ReadLine();
+                    password.Password = sr.ReadLine();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Ошибка сохранения", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void LogInClick(object sender, RoutedEventArgs e)
+        {
+            Regex regexEmail = new Regex(@"\A[^@]+@([^@\.]+\.)+[^@\.]+\z");
+            MatchCollection matchesEmail = regexEmail.Matches(email.Text);
+            if (matchesEmail.Count > 0 && email.Text.Length > 0)
+            {
+                if (password.Password.Length > 0)
+                {
+                    MySqlDataReader user = mainWindow.Query($"SELECT * FROM `parents` WHERE (email = '{email.Text}' AND password = '{password.Password}')");
+
+                    if (user == null) return;
+                    if (user.Read())
+                    {
+                        UserId.id = Convert.ToInt32(user.GetValue(0).ToString());
+
+                        mainWindow.userInfo.id = Convert.ToInt32(user.GetValue(0).ToString());
+                        mainWindow.userInfo.email = user.GetValue(1).ToString();
+                        mainWindow.userInfo.password = user.GetValue(2).ToString();
+                        mainWindow.userInfo.fio = user.GetValue(3).ToString();
+                        mainWindow.userInfo.phone = user.GetValue(4).ToString();
+                        mainWindow.userInfo.role = Convert.ToInt32(user.GetValue(5).ToString());
+
+                        if (save.IsChecked == true)
+                        {
+                            StreamWriter sw = new StreamWriter(mainWindow.localPath + @"\save.txt");
+                            sw.WriteLine(email.Text);
+                            sw.WriteLine(password.Password);
+                            sw.Close();
+                        }
+
+                        user.Close();
+                        if (mainWindow.userInfo.role == 0)
+                        {
+                            mainWindow.OpenPages(MainWindow.pages.main);
+                        }
+                        else if (mainWindow.userInfo.role == 1) mainWindow.OpenPages(MainWindow.pages.admin);
+                    }
+                    else MessageBox.Show("Такой пользователь не был найден.", "Warning", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                }
+                else MessageBox.Show("Введите пароль", "Warning", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            else MessageBox.Show("Введите логин", "Warning", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+
+        private void RegInClick(object sender, RoutedEventArgs e)
+        {
+            mainWindow.OpenPages(MainWindow.pages.registration);
+        }
+    }
+}
---
+++ Kursovoy/Pages/Registration.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+
+namespace Kursovoy.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        MainWindow mainWindow;
+        public Registration(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void RegIn(object sender, RoutedEventArgs e)
+        {
+            Regex regexEmail = new Regex(@"\A[^@]+@([^@\.]+\.)+[^@\.]+\z");
+            MatchCollection matchesEmail = regexEmail.Matches(email.Text);
+            if (matchesEmail.Count > 0 && email.Text.Length > 0)
+            {
+                Regex regexPassword = new Regex(@"([A-Z]|[0-9]){6,}");
+                MatchCollection matchesPassword = regexPassword.Matches(password.Text);
+                if (matchesPassword.Count > 0 && password.Text.Length > 0)
+                {
+                    if (password.Text == password_2.Text)
+                    {
+                        Regex regexFio = new Regex(@"[А-Яа-я]{3,}");
+                        MatchCollection matchesFio = regexFio.Matches(fio.Text);
+                        if (matchesFio.Count > 0 && fio.Text.Length > 0)
+                        {
+                            Regex regexPhone = new Regex(@"7[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]");
+                            MatchCollection matchesPhone = regexPhone.Matches(phone.Text);
+                            if (matchesPhone.Count > 0 && phone.Text.Length > 0)
+                            {
+                                mainWindow.Query($"INSERT INTO `parents`(`email`, `password`, `fio`, `phone`, `role`) VALUES ('{email.Text}','{password.Text}','{fio.Text}','{phone.Text}','0');");
+                                MessageBox.Show("Регистрация прошла успешно! Зайдите в аккаунт с главного окна!");
+                                mainWindow.OpenPages(MainWindow.pages.authorization);
+                            }
+                            else MessageBox.Show("Пожалуйста, укажите телефон в формате 70000000000", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                        }
+                        else MessageBox.Show("Пожалуйста, укажите ФИО на русском языке и без цифр", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                    }
+                    else MessageBox.Show("Пожалуйста, правильность введенных паролей", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                }
+                else MessageBox.Show("Пароль должен содержать латинские заглавные буквы, цифры, не менее 6 символов", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            else MessageBox.Show("Пожалуйста, укажите email в формате xxx@xxx.xx", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            mainWindow.OpenPages(MainWindow.pages.authorization);
+        }
+    }
+}
